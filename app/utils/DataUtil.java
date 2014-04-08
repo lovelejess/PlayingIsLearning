84315719c752c1ef3937f47ac7cdb57248e81c16
lovelejess <jessicale90@gmail.com>
2014-04-08 13:12:30 -0500

    layout and mobile compatibility edits

--- app/utils/DataUtil.java
+++ app/utils/DataUtil.java
@@ -21,11 +21,11 @@ public class DataUtil {
         try {
               
             /*PROD*/
-/*            MongoClient prodMongoInstance = new MongoClient("ds061787.mongolab.com", 61787); //PROD
+            MongoClient prodMongoInstance = new MongoClient("ds061787.mongolab.com", 61787); //PROD
             DB prodDB = prodMongoInstance.getDB("heroku_app15452455");
             prodDB.authenticate("icm_prod", "V3rify99".toCharArray()); //todo this needs to go in an ignored config file
             return prodDB;
-*/
+
             /*DEV*/
 //            MongoClient devMongoInstance = new MongoClient("ds035897.mongolab.com", 35897); //DEV
 //            DB devDB = prodMongoInstance.getDB("heroku_app23461665");
@@ -33,9 +33,9 @@ public class DataUtil {
 //            return devDB;
 
             /*LOCAL*/
-           MongoClient mongoClient = new MongoClient( );
+    /*       MongoClient mongoClient = new MongoClient( );
            DB localDB = mongoClient.getDB("icm");
-           return localDB;
+           return localDB;*/
 
 
         } catch (UnknownHostException e) {
